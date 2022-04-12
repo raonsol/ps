6a78f85d5519f7dc0d5ad0d911eb6c813a3692b4
raonsol <raonsol@kakao.com>
2022-04-12 22:44:40 +0900

    FIX: improved solutions for slicing vector

--- boj/class4/15649_permutation.cpp
+++ boj/class4/15649_permutation.cpp
@@ -7,19 +7,17 @@ int main() {
     int n, m;
     cin >> n >> m;
     vector<int> v(n);
-    vector<vector<int>> ans;
-
     for (int i = 0; i < n; i++) v[i] = i + 1;
-    do {
-        vector<int> tmp;
-        for (int i = 0; i < m; i++) tmp.push_back(v[i]);
-        ans.push_back(tmp);
-    } while (next_permutation(v.begin(), v.end()));
-    ans.erase(unique(ans.begin(), ans.end()), ans.end());
 
-    for (vector<int> i : ans) {
-        for (int j : i) cout << j << " ";
-        cout << '\n';
-    }
+    vector<int> tmp, ans;
+    do {
+        ans = vector<int>(v.begin(), v.begin() + m);
+        if (vector<int>(v.begin(), v.begin() + m) != tmp) {
+            for (int j : ans) cout << j << " ";
+            cout << '\n';
+            tmp = ans;
+        }
+    } while (next_permutation(v.begin(), v.end()));
+
     return 0;
 }
