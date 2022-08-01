c44331a86ed527952ebd6ab983680a3aaeeb3874
Byeongsu Kang <raonsol@kakao.com>
2022-08-02 01:59:31 +0900

    FIX: better solution

--- boj/class3/1074_dc.js
+++ boj/class3/1074_dc.js
@@ -3,30 +3,26 @@ const inputPath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 const [N, r, c] = fs.readFileSync(inputPath).toString().trim().split(" ").map(Number);
 const n = 2 ** N;
-let ans = -1;
-const zCheck = (y, x) => {
-  for (let i = y; i < y + 2; i++)
-    for (let j = x; j < x + 2; j++) {
-      ans++;
-      if (i === r && j === c) {
-        console.log(ans);
-        return;
-      }
-    }
-};
+let ans = 0;
+
 const traversal = (y, x, div) => {
-  let isTarget = false;
-  for (let i = y; i <= r; i += div / 2) {
-    for (let j = x; j < x + div; j += div / 2) {
-      if (div > 4) {
-        if (i + div / 2 > r && i <= r && j + div / 2 > c && j <= c) {
-          traversal(i, j, div / 2);
-        } else ans += (div * div) / 4;
-      } else isTarget = zCheck(i, j);
-      if (isTarget) return;
+  if (div === 1) {
+    ans += (r - y) * 2 + (c - x);
+  } else {
+    let part = JSON.stringify([parseInt((r - y) / div), parseInt((c - x) / div)]);
+    if (part === JSON.stringify([0, 0]))
+      traversal(y, x, div / 2);
+    else if (part === JSON.stringify([0, 1])) {
+      ans += div ** 2;
+      traversal(y, x + div, div / 2);
+    } else if (part === JSON.stringify([1, 0])) {
+      ans += div ** 2 * 2;
+      traversal(y + div, x, div / 2);
+    } else if (part === JSON.stringify([1, 1])) {
+      ans += div ** 2 * 3;
+      traversal(y + div, x + div, div / 2);
     }
   }
 };
-
-if (N > 1) traversal(0, 0, n);
-else zCheck(0, 0);
+traversal(0, 0, n / 2);
+console.log(ans);
