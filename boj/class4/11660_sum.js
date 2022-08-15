d83396df79f9161262182de4c636bba0912c6fba
Byeongsu Kang <raonsol@kakao.com>
2022-08-15 17:20:11 +0900

    FIX: better solution, 2x faster

--- boj/class4/11660_sum.js
+++ boj/class4/11660_sum.js
@@ -4,34 +4,23 @@ const inputPath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let [args, ...input] = fs.readFileSync(inputPath).toString().trim().split("\n");
 let [n, m] = args.split(" ").map(Number);
 
-let table = [];
-for (let i = 0; i < n; i++) {
-  let tmp = input[i].split(" ").map(Number);
-  tmp.unshift(0);
-  table.push(tmp);
-}
-let sumTable = new Array(n * (n + 1));
-let sum = 0;
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n + 1; j++) {
-    sum += table[i][j];
-    sumTable[(n + 1) * i + j] = sum;
-  }
-  sum = 0;
+let table = new Array(n+1);
+let sumTable = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+for (let i = 1; i <= n; i++) {
+  table[i] = [0, ...input[i - 1].split(" ").map(Number)];
+  for (let j = 1; j <= n; j++)
+    sumTable[i][j] = sumTable[i - 1][j] + sumTable[i][j - 1] - sumTable[i - 1][j - 1] + table[i][j];
 }
 
-let ans = "";
+let ans = [];
 for (let t = n; t < n + m; t++) {
   // 헷갈리므로 문제에서 y와 x를 바꿔서 입력받기
   let [y1, x1, y2, x2] = input[t].split(" ").map(Number);
-  sum = 0;
-  for (let i = y1; i <= y2; i++) {
-    sum += sumTable[(i - 1) * (n + 1) + x2] - sumTable[(i - 1) * (n + 1) + x1 - 1];
-  }
-  ans += `${sum}\n`;
+  ans.push(sumTable[y2][x2] - sumTable[y2][x1 - 1] - sumTable[y1 - 1][x2] + sumTable[y1 - 1][x1 - 1]);
 }
-console.log(ans.trim());
+console.log(ans.join('\n'));
 
-// 줄별로 누적합 구하기
-// (1,1)===1, (2,2)===7, (3,4)===14, n===4
-// (y-1)*(n+1)+x
+// (1,1)부터 (x, y)까지의 누적합 표 만들어 놓기
+// (x, y)는 (x-1, y)+(x, y-1)-(x-1, y-1)
+// (a, b)부터 (x, y)까지의 합은
+// sumTable[x][y]-sumTable[x][b-1]-sumTable[a-1][y]+sumTable[a-1][b-1]
