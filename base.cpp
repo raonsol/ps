11f9b0c14efb2b5a50a1264ec27da6a65af63109
Byeongsu Kang <raonsol@kakao.com>
2022-08-15 17:28:45 +0900

    FIX: use input.txt if local

--- base.cpp
+++ base.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define FAST_IO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define FAST_IO                \
+  ios::sync_with_stdio(false); \
+  cin.tie(NULL);               \
+  cout.tie(NULL);
 
 int main() {
-    FAST_IO;
-    int a, b, c;
-    cin >> a >> b >> c;
-    cout << a << b << c;
-    return 0;
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+#endif
+  FAST_IO;
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << a << b << c;
+  return 0;
 }
