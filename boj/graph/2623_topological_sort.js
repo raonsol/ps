9c16d9e7138f9604811c3915aa9393902c28fd8e
Byeongsu Kang <raonsol@kakao.com>
2022-12-07 00:48:09 +0900

    FIX: better speed solution

--- boj/graph/2623_topological_sort.js
+++ boj/graph/2623_topological_sort.js
@@ -3,33 +3,29 @@ const inputPath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 let [args, ...input] = fs.readFileSync(inputPath).toString().trim().split("\n");
 const [n, m] = args.split(" ").map(Number);
-const graph = Array.from(Array(n + 1), () => Array(n + 1).fill(false));
+const graph = Array.from(Array(n + 1), () => []);
 const connectedCnt = new Array(n + 1).fill(0);
 
 for (let line of input) {
   const [num, ...vertice] = line.split(" ").map(Number);
   for (let i = 1; i < num; i++) {
-    if (!graph[vertice[i - 1]][vertice[i]]) {
+    if (!graph[vertice[i - 1]].includes(vertice[i])) {
       connectedCnt[vertice[i]]++;
-      graph[vertice[i - 1]][vertice[i]] = true;
+      graph[vertice[i - 1]].push(vertice[i]);
     }
   }
 }
 
-let q = [],
-  qStart = 0;
+let q = [], qStart = 0;
 for (let i = 1; i <= n; i++) {
   if (!connectedCnt[i]) q.push(i);
 }
 
 while (q.length != qStart) {
   const target = q[qStart++];
-  for (let i = 1; i <= n; i++) {
-    if (graph[target][i]) {
-      graph[target][i] = false;
-      connectedCnt[i]--;
-      if (!connectedCnt[i]) q.push(i);
-    }
+  for (let i of graph[target]) {
+      if (connectedCnt[i] === 1) q.push(i);
+      else connectedCnt[i]--;
   }
 }
 const ans = q.length == n ? q.join("\n").trim() : "0";
